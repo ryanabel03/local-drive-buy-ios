427dd96003e162c87b167b8e49a6877d9ecc3770
Ryan Abel <abelr@mail.gvsu.edu>
2013-02-04 16:17:08 -0500

    Initialized empty rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LocalDriveBuyIos::Application.config.secret_token = '0b89438ca69cd34ff2e1278e7bdf87727625952a8a84f2cba3be1a873343deb254078891d11f73b3f5311a8fe900e27f5a815e29b295e2d77d9e8ab4bd94909b'
